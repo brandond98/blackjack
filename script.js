9200f99cbcea935403e568bd9dadf704d17b50e6
Brandon Dickson <bdickson1212@gmail.com>
2021-06-17 20:16:24 +0100

    added a deck generating function and added some tarting parameters

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 // Initialise player score, dealer score, deck
+let playerScore = 0;
+let dealerScore = 0;
+let deck = [];
+const suits = ["spades", "hearts", "diamonds", "clubs"];
+const ranks = [
+  "ace",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+  "jack",
+  "queen",
+  "king",
+];
 
 //Start game
 
 //Create deck
+const createDeck = () => {
+  ranks.forEach((rank) => {
+    suits.forEach((suit) => {
+      deck.push(`${rank} of ${suit}`);
+    });
+  });
+};
 
-//Shuffle deck
+createDeck();
 
 //Deal cards to player and computer
 
