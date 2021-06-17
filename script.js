8e368021b2df6c50360b9e1422af6ed87cafe639
Brandon Dickson <bdickson1212@gmail.com>
2021-06-17 13:58:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Initialise player score, dealer score, deck
+
+//Start game
+
+//Create deck
+
+//Shuffle deck
+
+//Deal cards to player and computer
+
+//Hit/Stand funtionality - remove cards from deck/add up scores - ACE 11 or 1 functionality
+
+//Determine who won game
+
+//Replay
