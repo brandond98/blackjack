557ed9f8902349e5aa4b18d2514d3f3e19ff361a
Brandon Dickson <bdickson1212@gmail.com>
2021-06-22 12:04:42 +0100

    added calculate score function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Initialise player score, dealer score, deck
+// Initialise player score, dealer score, deck, hands
 let playerScore = 0;
 let dealerScore = 0;
 let deck = [];
@@ -49,6 +49,10 @@ const shuffleDeck = (deck) => {
 const startGame = () => {
   playerScore = 0;
   dealerScore = 0;
+  deck = [];
+  playersHand = [];
+  dealersHand = [];
+
   createDeck();
   shuffleDeck(deck);
   dealHands();
@@ -57,11 +61,41 @@ const startGame = () => {
 //Deal cards to player and computer
 const dealHands = () => {
   for (let i = 0; i < 2; i++) {
-    const playerCard = playersHand.push(deck[0]);
+    const playerCard = deck[0];
+    playersHand.push(playerCard);
+    $("#players-cards").append(`<li>${playerCard}</li>`);
+    deck.shift();
 
+    const dealerCard = deck[0];
+    playersHand.push(dealerCard);
+    $("#dealers-cards").append(`<li>${dealerCard}</li>`);
     deck.shift();
-    const dealerCard = dealersHand.push(deck[0]);
-    deck.shift();
+  }
+};
+
+const calculateScore = (card) => {
+  if (card === "ace") {
+    return 1;
+  } else if (card === "one") {
+    return 1;
+  } else if (card === "two") {
+    return 2;
+  } else if (card === "three") {
+    return 3;
+  } else if (card === "four") {
+    return 4;
+  } else if (card === "five") {
+    return 5;
+  } else if (card === "six") {
+    return 6;
+  } else if (card === "seven") {
+    return 7;
+  } else if (card === "eight") {
+    return 8;
+  } else if (card === "nine") {
+    return 5;
+  } else {
+    return 10;
   }
 };
 
