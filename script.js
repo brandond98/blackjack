42e3a87ebd953d9de8431dd40e73d538529f8188
Brandon Dickson <bdickson1212@gmail.com>
2021-06-22 11:39:44 +0100

    added shuffle deck & changed deal hands function

--- script.js
+++ script.js
@@ -22,22 +22,6 @@ const ranks = [
   "king",
 ];
 
-//Start game
-const startGame = () => {
-  playerScore = 0;
-  dealerScore = 0;
-  dealHands();
-};
-
-const dealHands = () => {
-  for (let i = 0; i < 2; i++) {
-    const playerCard = deck[Math.round(Math.random() * 52)];
-    const dealerCard = deck[Math.round(Math.random() * 52)];
-    playersHand.push(playerCard);
-    dealersHand.push(dealerCard);
-  }
-};
-
 //Create deck
 const createDeck = () => {
   ranks.forEach((rank) => {
@@ -47,9 +31,39 @@ const createDeck = () => {
   });
 };
 
-createDeck();
+//Shuffle deck - code copied from https://www.geeksforgeeks.org/how-to-shuffle-an-array-using-javascript/
+const shuffleDeck = (deck) => {
+  for (let i = deck.length - 1; i > 0; i--) {
+    // Generate random number
+    const j = Math.floor(Math.random() * (i + 1));
+
+    const temp = deck[i];
+    deck[i] = deck[j];
+    deck[j] = temp;
+  }
+
+  return deck;
+};
+
+//Start game
+const startGame = () => {
+  playerScore = 0;
+  dealerScore = 0;
+  createDeck();
+  shuffleDeck(deck);
+  dealHands();
+};
 
 //Deal cards to player and computer
+const dealHands = () => {
+  for (let i = 0; i < 2; i++) {
+    const playerCard = playersHand.push(deck[0]);
+
+    deck.shift();
+    const dealerCard = dealersHand.push(deck[0]);
+    deck.shift();
+  }
+};
 
 //Hit/Stand funtionality - remove cards from deck/add up scores - ACE 11 or 1 functionality
 
